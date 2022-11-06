b4503c2d6847b8df6d98f7ef99d5a3233ce5bab1
Yaksh-10 <103356610+Yaksh-10@users.noreply.github.com>
2022-11-06 16:33:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+https://teachablemachine.withgoogle.com/models/LkYmYSzRp/
+
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:'png',
+    image_quality:100
+});
+
+camera = document.getElementById('camera');
+Webcam.attach('#camera')
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById('result').innerHTML = '<img id="captured_image" src="'+data_uri+'">'
+    });
+}
+console.log('ml5.version', ml5.version)
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LkYmYSzRp/model.json', modelloaded)
+
+function modelloaded()
+{
+    console.log('model loaded');
+}
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error,results){
+    if (error){
+        console.error(error)
+    }
+      else{
+        console.log(results);
+        document.getElementById('object_name').innerHTML = results[0].label;
+        document.getElementById('object_accuracy').innerHTML = results[0].confidence.toFixed(2); 
+      }
+}
